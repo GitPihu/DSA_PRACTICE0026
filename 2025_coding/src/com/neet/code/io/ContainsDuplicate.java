ca1ba46bef80ac6ad395028c377f6d8c8dcb0134
Priyanka <Priyanka@LAPTOP-TFQ9NHF5>
2026-01-05 00:31:26 +0530

    dfsdf

--- 2025_coding/src/com/neet/code/io/ContainsDuplicate.java
+++ 2025_coding/src/com/neet/code/io/ContainsDuplicate.java
@@ -12,21 +12,33 @@ public class ContainsDuplicate {
 	public boolean hasDuplicate(int[] nums) {
 
 		// using set
-		boolean hasDuplicate;
-		Set<Integer> st = new HashSet<Integer>();
-		hasDuplicate = Arrays.stream(nums).anyMatch(n -> !st.add(n));
+//		boolean hasDuplicate;
+//		Set<Integer> st = new HashSet<Integer>();
+//		hasDuplicate = Arrays.stream(nums).anyMatch(n -> !st.add(n));
+//
+//		if (hasDuplicate) {
+//			return hasDuplicate;
+//
+//		}
+//		return hasDuplicate;
 
-		if (hasDuplicate) {
-			return hasDuplicate;
+		// using brute force
 
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i+1; j < nums.length; j++) {
+
+				if (nums[i] == nums[j]) {
+					return true;
+				}
+			}
 		}
-		return hasDuplicate;
+		return false;
 	}
 
 	public static void main(String args[]) {
 		ContainsDuplicate obj = new ContainsDuplicate();
-		int arr[] = { };
-		boolean b=obj.hasDuplicate(null);
+		int arr[] = {4,1,2,3,4};
+		boolean b = obj.hasDuplicate(arr);
 		System.out.print(b);
 	}
 }
