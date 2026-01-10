b57a118aaf9039e3868f6feb531453b91edaf752
Priyanka <Priyanka@LAPTOP-TFQ9NHF5>
2026-01-10 10:08:21 +0530

    h

--- 2025_coding/src/com/neet/code/io/ContainsDuplicate.java
+++ 2025_coding/src/com/neet/code/io/ContainsDuplicate.java
@@ -25,7 +25,7 @@ public class ContainsDuplicate {
 		// using brute force
 
 		for (int i = 0; i < nums.length; i++) {
-			for (int j = i+1; j < nums.length; j++) {
+			for (int j = i + 1; j < nums.length; j++) {
 
 				if (nums[i] == nums[j]) {
 					return true;
@@ -37,7 +37,7 @@ public class ContainsDuplicate {
 
 	public static void main(String args[]) {
 		ContainsDuplicate obj = new ContainsDuplicate();
-		int arr[] = {4,1,2,3,4};
+		int arr[] = { 4, 1, 2, 3, 4 };
 		boolean b = obj.hasDuplicate(arr);
 		System.out.print(b);
 	}
